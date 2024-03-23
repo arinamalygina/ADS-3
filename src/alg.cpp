f6b883652458f97e7f297422a4848fb740071803
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-03-23 14:40:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,4 +3,35 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
+  int cbinsearch(int* arr, int size, int value) {
+  int count = 0;
+  int left = 0;
+  int right = size - 1;
+
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == value) {
+      count++;
+
+      int i = mid - 1;
+      while (i >= 0 && arr[i] == value) {
+        count++;
+        i--;
+      }
+      int j = mid + 1;
+      while (j < size && arr[j] == value) {
+        count++;
+        j++;
+      }
+      break;
+    } else if (arr[mid] < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return count;
 }
+
